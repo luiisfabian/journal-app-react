3d51a8d5305271f5b2567d94d8b309af5330a5ad
FabianM <luiisfabian96@gmail.com>
2024-02-03 01:47:05 -0500

    Implmentacion de login PAge sin Layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Grid, TextField, Typography } from '@mui/material'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid container spacing={0} direction='column' alignItems='center' justifyContent='center'
+      sx={{ minHeight: '100hv', backgroundColor: 'primary.main', padding: 4 }}>
+
+      <Grid item className='box-shadow' xs={3} sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+
+        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
+
+        <form>
+          <Grid container>
+             <Grid item xs={12} sx={{mt: 5}}>
+                <TextField label="Email" type='email' placeholder='email@email.com' fullWidth />
+             </Grid>
+
+             <Grid item xs={12} sx={{mt: 5}}>
+                <TextField label="Password" type='password' placeholder='e*****' fullWidth />
+             </Grid>
+            
+          </Grid>
+        </form>
+
+      </Grid>
+
+    </Grid>
   )
 }
