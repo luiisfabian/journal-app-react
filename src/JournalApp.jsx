863382ca0980423c4e6b8fa346d34b9141238bbe
FabianM <luiisfabian96@gmail.com>
2024-02-03 01:10:05 -0500

    configuracion de material Iu con Vite

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,13 @@
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme"
 
 export const JournalApp = () => {
   return (
     <>
-    <AppRouter />
+      <AppTheme>
+        <AppRouter />
+
+      </ AppTheme >
     </>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { MailOutline } from '@mui/icons-material'
+import { Typography } from '@mui/material'
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+    
+    <Typography variant='h1'>JournalPage</Typography>
+    <MailOutline/>
+
+</>
+
   )
 }
