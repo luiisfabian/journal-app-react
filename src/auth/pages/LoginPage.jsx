33230cc7c546ea56072bb187dbdc7bcf9bb5a073
FabianM <luiisfabian96@gmail.com>
2024-02-05 07:01:36 -0500

    trabajando en la pestaña de LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
 
 export const LoginPage = () => {
   return (
@@ -11,14 +13,41 @@ export const LoginPage = () => {
 
         <form>
           <Grid container>
-             <Grid item xs={12} sx={{mt: 5}}>
-                <TextField label="Email" type='email' placeholder='email@email.com' fullWidth />
-             </Grid>
+            <Grid item xs={12} sx={{ mt: 5 }}>
+              <TextField label="Email" type='email' placeholder='email@email.com' fullWidth />
+            </Grid>
 
-             <Grid item xs={12} sx={{mt: 5}}>
-                <TextField label="Password" type='password' placeholder='e*****' fullWidth />
-             </Grid>
-            
+            <Grid item xs={12} sx={{ mt: 5 }}>
+              <TextField label="Password" type='password' placeholder='e*****' fullWidth />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mb: '2', mt: 5 }}>
+
+              <Grid item xs={12} sm={3}>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+
+              <Grid item xs={12} sm={3}>
+                <Button variant='contained' fullWidth>
+                  <Google />
+                  <Typography sx={{ marginLeft: 1 }} >
+                    Google
+
+                  </Typography>
+                </Button>
+              </Grid>
+
+            </Grid>
+
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Link component={RouterLink} color='inherit' to="/auth/register">
+              Crear una Cuenta
+
+            </Link>
           </Grid>
         </form>
 
