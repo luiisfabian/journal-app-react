8c54944fa1af40badebd4442cd771c068dbd0d82
FabianM <luiisfabian96@gmail.com>
2024-03-16 01:41:14 -0500

    TRabjanado en los errores en pantalla

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,6 +2,7 @@ import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
 import { Layout } from '../layout/Layout'
 import { useForm } from '../../hooks'
+import { useState } from 'react'
 
 
 const formData = {
@@ -20,14 +21,15 @@ const formValidations = {
 
 export const RegisterPage = () => {
 
+  const [formSubmitted, setFormSubmitted] = useState(false)
 
-  const { displayName, email, password, onInputChange, formState, isFormValid, emailValid, passwordValid, displayNameValid } = useForm(formData);
+  const { displayName, email, password, onInputChange, formState, isFormValid, emailValid, passwordValid, displayNameValid} = useForm(formData, formValidations);
 
   console.log(displayNameValid);
 
   const onSubmit = (event) => {
     console.log(formState);
-
+   setFormSubmitted(true);
     event.preventDefault()
 
   }
@@ -37,20 +39,20 @@ export const RegisterPage = () => {
 
 
     <Layout title='Registro'>
-
+      <h1>FormValid {isFormValid ? 'Valido': 'Incorrecto'}</h1>
       <form>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 5 }}>
-            <TextField helperText={displayNameValid} error={!displayNameValid} name='displayName' value={displayName} onChange={onInputChange} label="Nombre Completo" type='text' placeholder='Nombre' fullWidth />
+            <TextField helperText={displayNameValid} error={!!displayNameValid && formSubmitted} name='displayName' value={displayName} onChange={onInputChange} label="Nombre Completo" type='text' placeholder='Nombre' fullWidth />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 5 }}>
-            <TextField helperText={emailValid} error={!emailValid} name='email' value={email} onChange={onInputChange} label="Email" type='text' placeholder='email@email.com' fullWidth />
+            <TextField helperText={emailValid} error={!!emailValid && formSubmitted} name='email' value={email} onChange={onInputChange} label="Email" type='text' placeholder='email@email.com' fullWidth />
           </Grid>
 
 
           <Grid item xs={12} sx={{ mt: 5 }}>
-            <TextField helperText={passwordValid} error={!passwordValid} name='password' value={password} onChange={onInputChange} label="Password" type='password' placeholder='e*****' fullWidth />
+            <TextField helperText={passwordValid} error={!!passwordValid && formSubmitted} name='password' value={password} onChange={onInputChange} label="Password" type='password' placeholder='e*****' fullWidth />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: '2', mt: 5 }}>
