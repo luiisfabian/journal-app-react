403eb8917aae78459167232e9d6e35d1a0761999
FabianM <luiisfabian96@gmail.com>
2024-03-28 12:27:04 -0500

    creacion de checkingAutentication

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage, RegisterPage } from '../pages'
+import { useSelector } from 'react-redux'
+import { CheckingAuth } from '../../ui/components/CheckingAuth'
 
 export const AuthRoutes = () => {
+
+  const { status } = useSelector(state =>  state.auth)
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+    
+  }
   return (
     <Routes>
             <Route path='login' element={<LoginPage />}></Route>
